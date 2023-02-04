36e03452034568cc3435dd88436c8152c4d9f7d3
Alexey_I <gafar@xaker.ru>
2023-02-04 17:50:39 +0300

    Itogovy script

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+ 
+namespace factorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число: ");
+            int chisloN = int.Parse(Console.ReadLine());
+            int i = 1;
+            double factorial = 1;
+            while ( i <= chisloN)
+            {
+                factorial = factorial * i;
+                i = i + 1;
+            }
+            Console.WriteLine("Факториал числа " + chisloN + " равен " + factorial);
+        }
+    }
+}
